e8c2383d3460064c2da45507308b50e6514b804e
CK Do <ckdo8008@gmail.com>
2024-06-05 00:27:34 +0900

    컴포트 변경

--- include/ros2-nurirobot-driver/config.hpp
+++ include/ros2-nurirobot-driver/config.hpp
@@ -1,10 +1,10 @@
 #pragma once
 
-#define PORT "/dev/ttyUSB0"
+#define PORT "/dev/ttyUSB1"
 #define DIRECTION_CORRECTION 1
 #define MAX_LIN_VEL_X 2.0f
 #define MAX_ANG_VEL_Z 2.5f
 //#define WHEEL_SEPARATION 0.38f
 //#define WHEEL_RADIUS 0.102887f
 #define WHEEL_SEPARATION 0.45f
-#define WHEEL_RADIUS 0.025
+#define WHEEL_RADIUS 0.01
